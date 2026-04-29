b0da8f709018e97fb8ffdd18618148cfb210de9f
boltography <cole12bauman@gmail.com>
2026-04-29 15:06:01 -0700

    Implement basic socket client in C++

---
+++ socket_client.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+WSADATA wsa;
+WSAStartup(MAKEWORD(2,2), &wsa);
+
+SOCKET client = socket(AF_INET, SOCK_STREAM, 0);
+
+sockaddr_in server;
+server.sin_family = AF_INET;
+server.sin_port = htons(8080);
+server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+connect(client, (sockaddr*)&server, sizeof(server));
+
+const char* msg = "UP";
+send(client, msg, strlen(msg), 0);
+
+closesocket(client);
+WSACleanup();
+}
